3f67492f2656910d39dd99c4c1044fd4b61e218c
JayTalekar <jaytalekar.developer@gmail.com>
2020-08-29 22:59:05 +0530

    Created Room Database by extending it on SleepDatabase class.

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,52 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// To tell which entity to use use the parameters with Annotation
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    // Intialize the DAO related with the entity, so to interact with the Database
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    //Companion Object to access the database without instantiating the class
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        // A method to get the Database instance
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+
+                    // Room's Database builder to instantiate the local variable
+                    /*Migration fallback means that if user has upgraded the app
+                      with newer schema of database then the database builder will
+                      make the instance with newer schema by convering the old schema
+                      to new schema, so that users don't lose data on upgrading the app*/
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    ).fallbackToDestructiveMigration().build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+
+            }
+
+        }
+    }
+
+
+}
